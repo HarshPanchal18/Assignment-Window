1d95e59dfe117300e4a197d07d4e6b3a27ad6d97
Harsh Panchal <harshhhh1803@gmail.com>
2024-09-10 12:59:44 +0530

    Created testcase class and Question class

---
+++ AssignmentConsole/Model/Question.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AssignmentConsole.Model {
+    internal class Question {
+        public int Id { get; set; }
+
+        public int? AssignmentId { get; set; }
+
+        public string Description { get; set; }
+
+        public bool? Status { get; set; }
+
+        public bool Deleted { get; set; } = false;
+
+        public DateTime? CreationTimestamp { get; set; }
+
+        public DateTime? UpdationTimestamp { get; set; }
+
+        public DateTime? DeletionTimestamp { get; set; }
+
+        public string FunctionSignature { get; set; }
+
+        public int ParameterCount { get; set; } = 0;
+
+        public virtual Assignment Assignment { get; set; }
+
+        public virtual ICollection<Testcase> Testcases { get; set; } = new List<Testcase>();
+
+    }
+}
---
+++ AssignmentConsole/Model/Testcase.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace AssignmentConsole.Model {
+    internal class Testcase<T1, T2, T3> {
+
+        // public Testcase(params  input) { }
+
+        public int Id { get; set; }
+
+        public int QuestionId { get; set; }
+
+        public string InputValues { get; set; } = null;
+
+        public string InputValueDatatype { get; set; } = null;
+
+        public bool IsHidden { get; set; } = false;
+
+        public string OutputValue { get; set; } = null;
+
+        public string OutputValueDatatype { get; set; } = null;
+
+        public bool Status { get; set; } = false;
+
+        public bool Deleted { get; set; } = false;
+
+        public DateTime? CreationTimestamp { get; set; }
+
+        public DateTime? UpdationTimestamp { get; set; }
+
+        public DateTime? DeletionTimestamp { get; set; }
+
+        public virtual Question Question { get; set; } = null;
+
+        /*public void Demo() {
+            List<Testcase> testCases = new List<Testcase> {
+                new Testcase(1, 2, 3),
+                new Testcase(5, 7, 12),
+                new Testcase(-1, -1, -2),
+                new Testcase(0, 0, 0)
+            };
+
+            foreach (var testcase in testcases) {
+                int result = Solution.Add(testcase.Input1, testcase.Input2);
+
+                if (result == testcase.ExpectedOutput) {
+                    Console.WriteLine($"Test passed for inputs ({testcase.Input1}, {testcase.Input2})");
+                } else {
+                    Console.WriteLine($"Test failed for inputs ({testcase.Input1}, {testcase.Input2}). Expected {testcase.ExpectedOutput}, but got {result}");
+                }
+            }
+        }*/
+
+    }
+
+}
