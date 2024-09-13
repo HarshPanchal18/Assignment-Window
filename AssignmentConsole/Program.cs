bad8b3f87db7ba0128d89d985641e48e7fcc8459
Harsh Panchal <harshhhh1803@gmail.com>
2024-09-13 14:55:21 +0530

    Extracted Runner methods to class

--- AssignmentConsole/Program.cs
+++ AssignmentConsole/Program.cs
@@ -14,8 +14,9 @@ namespace AssignmentConsole {
     internal class Program {
 
         static void Main(string[] args) {
+            Runner runner = new Runner();
 
-            RunCsCode(
+            runner.RunCsCode(
                         Const.USING_IMPORTS +
 
                         Const.MainMethod(
@@ -31,51 +32,6 @@ namespace AssignmentConsole {
 
             );
         }
-
-        private static Action<string> RunCsCode = (sourceCode) => {
-            char[] whitespace = new char[] { ' ', '\t', '\r', '\n' };
-
-            try {
-
-                // Trimming whitespaces from the source code.
-                string[] trimmedCode = sourceCode.Split(whitespace, StringSplitOptions.RemoveEmptyEntries);
-
-                sourceCode = string.Join(" ", trimmedCode);
-
-                // Setting up the compiler
-                using (var provider = new CSharpCodeProvider()) {
-                    var parameters = new CompilerParameters {
-                        GenerateExecutable = true,
-                        GenerateInMemory = true,
-                    };
-
-                    // add a reference to the 'System.dll' assembly, necessary for the code to run.
-                    parameters.ReferencedAssemblies.Add("System.dll");
-
-                    // Compiling the source-code into an assembly
-                    CompilerResults results = provider.CompileAssemblyFromSource(parameters, sourceCode);
-
-                    if (results.Errors.Count > 0) { // Compilation error
-                        foreach (CompilerError error in results.Errors) {
-                            Console.WriteLine($"Error ({error.ErrorNumber}): {error.ErrorText}");
-                        }
-                    } else {
-                        Assembly assembly = results.CompiledAssembly;
-                        Type programType = assembly.GetType("Program"); // ClassName of the source-code which contains Main()
-                        MethodInfo mainMethod = programType.GetMethod("Main"); // Provide entry point method name to start from
-
-                        mainMethod.Invoke(null, null); // Invoke the Main method
-                    }
-                }
-
-            } catch (Win32Exception ex) {
-                Console.WriteLine("System error...");
-                Console.WriteLine(ex.Message);
-            } catch (Exception ex) {
-                Console.WriteLine("Unhandled Exception occurred...");
-                Console.WriteLine(ex.Message);
-            }
-        };
 
         public static void AdditionTestcase() {
             List<Testcase<int>> testCases = new List<Testcase<int>> {
@@ -122,30 +78,10 @@ namespace AssignmentConsole {
                 } else {
                     Console.WriteLine($"Test failed for inputs ({string.Join(", ", testcase.Inputs.Select(i => i.Item2))}). " +
                         $"Expected {testcase.Output}, but got {result}");
+
                 }
             }
         }
 
-
-        static void runCprogram() {
-
-            ProcessStartInfo startInfo = new ProcessStartInfo() {
-                UseShellExecute = true,
-                RedirectStandardOutput = true,
-                RedirectStandardError = true,
-                RedirectStandardInput = true
-            };
-
-            Process ps = Process.Start(startInfo);
-
-            string output = ps.StandardOutput.ReadToEnd();
-            Console.WriteLine(output);
-
-            string error = ps.StandardError.ReadToEnd();
-            Console.WriteLine(error);
-
-            ps.WaitForExit();
-
-        }
     }
 }
---
+++ AssignmentConsole/Utils/Runner.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.CSharp;
+using System;
+using System.CodeDom.Compiler;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace AssignmentConsole.Utils {
+    public class Runner {
+
+        public Runner() { }
+
+        public Action<string> RunCsCode = (sourceCode) => {
+            char[] whitespace = new char[] { ' ', '\t', '\r', '\n' };
+
+            try {
+
+                // Trimming whitespaces from the source code.
+                string[] trimmedCode = sourceCode.Split(whitespace, StringSplitOptions.RemoveEmptyEntries);
+
+                sourceCode = string.Join(" ", trimmedCode);
+
+                // Setting up the compiler
+                using (var provider = new CSharpCodeProvider()) {
+                    var parameters = new CompilerParameters {
+                        GenerateExecutable = true,
+                        GenerateInMemory = true,
+                    };
+
+                    // add a reference to the 'System.dll' assembly, necessary for the code to run.
+                    parameters.ReferencedAssemblies.Add("System.dll");
+
+                    // Compiling the source-code into an assembly
+                    CompilerResults results = provider.CompileAssemblyFromSource(parameters, sourceCode);
+
+                    if (results.Errors.Count > 0) { // Compilation error
+                        foreach (CompilerError error in results.Errors) {
+                            Console.WriteLine($"Error ({error.ErrorNumber}): {error.ErrorText}");
+                        }
+                    } else {
+                        Assembly assembly = results.CompiledAssembly;
+                        Type programType = assembly.GetType("Program"); // ClassName of the source-code which contains Main()
+                        MethodInfo mainMethod = programType.GetMethod("Main"); // Provide entry point method name to start from
+
+                        mainMethod.Invoke(null, null); // Invoke the Main method
+                    }
+                }
+
+            } catch (Win32Exception ex) {
+                Console.WriteLine("System error...");
+                Console.WriteLine(ex.Message);
+            } catch (Exception ex) {
+                Console.WriteLine("Unhandled Exception occurred...");
+                Console.WriteLine(ex.Message);
+            }
+        };
+
+        public void runCprogram() {
+
+            ProcessStartInfo startInfo = new ProcessStartInfo() {
+                UseShellExecute = true,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                RedirectStandardInput = true
+            };
+
+            Process ps = Process.Start(startInfo);
+
+            string output = ps.StandardOutput.ReadToEnd();
+            Console.WriteLine(output);
+
+            string error = ps.StandardError.ReadToEnd();
+            Console.WriteLine(error);
+
+            ps.WaitForExit();
+
+        }
+
+    }
+}
