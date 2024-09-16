87d1f590ea6a0e79a0bb34bcb821de0a504902f7
Harsh Panchal <harshhhh1803@gmail.com>
2024-09-16 17:17:48 +0530

    Tested cases for int array types

--- AssignmentConsole/Program.cs
+++ AssignmentConsole/Program.cs
@@ -33,6 +33,10 @@ namespace AssignmentConsole {
 
             EvenListTestcase();
 
+            Console.WriteLine();
+
+            EvenArrayTestcase();
+
         }
 
         public static void AdditionTestcase() {
@@ -117,6 +121,32 @@ namespace AssignmentConsole {
                 var list = testcase.Inputs[0].Item2.Split(',').Select(int.Parse).ToList();
 
                 var result = Solution.GetEvens(list);
+
+                // Use SequenceEqual to compare lists
+                if (result.SequenceEqual(testcase.Output)) {
+                    Console.WriteLine(
+                        $"Test passed for inputs ({testcase.Inputs[0].Item2}). " +
+                        $"Got [{string.Join(", ", result)}]."
+                    );
+                } else {
+                    Console.WriteLine(
+                        $"Test failed for inputs ({testcase.Inputs[0].Item2}). " +
+                        $"Expected {testcase.Output}, but got [{string.Join(", ", result)}]."
+                    );
+                }
+            }
+        }
+        public static void EvenArrayTestcase() {
+            List<Testcase<int[]>> testCases = new List<Testcase<int[]>> {
+                new Testcase<int[]>(new int[] {8,0,2}, (DataType.INT_ARR, "8,0,7,2")),
+                new Testcase<int[]>(new int[] {8,6,0}, (DataType.INT_ARR, "8,6,7,0")),
+                new Testcase<int[]>(new int[] {}, (DataType.INT_ARR, "9,7,1")),
+            };
+
+            foreach (var testcase in testCases) {
+                var list = testcase.Inputs[0].Item2.Split(',').Select(int.Parse).ToArray();
+
+                var result = Solution.GetEvens(list.ToList());
 
                 // Use SequenceEqual to compare lists
                 if (result.SequenceEqual(testcase.Output)) {
